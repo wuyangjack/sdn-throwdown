0b65119ce27308a71c8ef879fe864c0244527ba1
Qingxiao Dong <qingxiao.dong@gmail.com>
2016-02-25 22:57:15 -0500

    implement database functions

--- frontend/react-tutorial/states.py
+++ frontend/react-tutorial/states.py
@@ -11,7 +11,7 @@ class NetworkStateService(object):
 
 	def create(self, name):
 		c = self.connection.cursor();
-		c.execute("CREATE TABLE " + name + " (name text, key text, time real, value real)");
+		c.execute("CREATE TABLE " + name + " (name text, key text, time real, value real, PRIMARY KEY(name, key, time))");
 
 	def __init__(self, database):
 		self.connection = sqlite3.connect(database);
@@ -24,25 +24,37 @@ class NetworkStateService(object):
 			self.tables.append(row[0])
 
 	def save(self, name, key, time, value):
-		# if name in self.table:
-		# 	c.execute("INSERT INTO " + name + " VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
-		# else:
-
-		print "save to database"
+		c = self.connection.cursor();
 
 		# Create table
+		if name not in self.tables:
+			self.create(name);
 
 		# Insert a row of data
 		#c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+		c.execute("INSERT OR REPLACE INTO " + name + " VALUES ('" + name + "','" + key + "','" + time + "','" + value + "')");
 
+		print "save to database"
+	
 		# Save (commit) the changes
-		#conn.commit()
+		self.connection.commit()
 
 	def query(self, query):
-		print "execute query"
+		c = self.connection.cursor();
+		print "executing query: " + query;
+		rows = c.execute(query);
+		for row in rows:
+			print row;
+
+	def clear(self):
+		c = self.connection.cursor();
+		c.execute("DROP TABLE LinkStatus");
+		c.execute("DROP TABLE LinkUtilization");
+		self.connection.commit()
 
 nss = NetworkStateService("./example.db");
 nss.save("LinkStatus", "10.0.0.1_10.0.0.2", "1456451402", "Up");
-#nss.save("LinkUtilization", "10.0.0.1_10.0.0.2", "1456451402", "0.04");
-#nss.query("SELECT * FROM LinkUtilization");
-#nss.query("SELECT * FROM LinkStatus");
+nss.save("LinkUtilization", "10.0.0.1_10.0.0.2", "1456451402", "0.04");
+nss.query("SELECT * FROM LinkUtilization");
+nss.query("SELECT * FROM LinkStatus");
+#nss.clear();
