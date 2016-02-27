3f1a4f8a289473db4bd3c221959eafd5ec6f6ad1
Yunchen Wei <weiyunchen0807@outlook.com>
2016-02-27 15:44:56 -0500

    Refractoring code

--- code/backend/redis_test.py
+++ code/backend/redis_test.py
@@ -13,12 +13,5 @@ def getInputBPS(text):
 
 
 r = redis.StrictRedis(host='10.10.4.252', port=6379, db=0)
-# print (r.lrange("new york:ge-1/0/7:traffic statistics", 0, 0)[0])
 print "MIAMI OUT: " + getOutputBPS(r.lrange("miami:ge-0/1/3:traffic statistics", 0, 0)[0])
-# print "HOUSTON IN: " + (r.lrange("houston:ge-0/1/2:traffic statistics", 0, 0)[0])
-# print "HOUSTON OUT: " + (r.lrange("houston:ge-0/1/1:traffic statistics", 0, 0)[0])
 print "DALLAS IN: " + getInputBPS(r.lrange("dallas:ge-1/0/3:traffic statistics", 0, 0)[0])
-
-# nss.save(NetworkStateService.Interface, key, timestamp, 1)
-# nss.save(NetworkStateService.InterfaceInBps, key, timestamp, itfcTraffic.inputBPS)
-# nss.save(NetworkStateService.InterfaceOutBps, key, timestamp, itfcTraffic.outputBPS)
