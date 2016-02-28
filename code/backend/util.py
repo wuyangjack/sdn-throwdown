1ac0d5d92bd762461c17045b335ff2405e44416f
Yang (Jack) Wu <yangwu6@cis.upenn.edu>
2016-02-28 02:47:38 -0500

    Add

--- code/backend/util.py
+++ code/backend/util.py
@@ -48,7 +48,7 @@ def updateTopology():
                 trafficStatDict[address].log(nss)
 
             graph = Graph(nodeDict.values(), linkDict)
-            #updateBadLinks(linkDict, graph, LSPs, 0.5)
+            updateBadLinks(linkDict, graph, LSPs, 0.5)
 
             data = {'timestamp': ts, 'nodes': nodeDict.values(), 'links': linkDict.values(), 'lsps': LSPs}
 
